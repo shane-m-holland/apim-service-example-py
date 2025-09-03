c3c5e48aa56eb3fe9b7061a035cba2082b4bb2c6
Shane Holland <shane.holland@ahead.com>
2025-09-03 08:20:04 -0400

    Test API deployment from generated spec

--- export_openapi_yaml.py
+++ export_openapi_yaml.py
@@ -3,5 +3,5 @@ from main import app
 
 if __name__ == "__main__":
     spec = app.openapi()
-    with open("openapi.yaml", "w") as f:
+    with open("./specs/openapi.yaml", "w") as f:
         yaml.dump(spec, f, sort_keys=False)
