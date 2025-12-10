948deecd19b4d6ec4a362bb619309aef5d1c9827
Shane Holland <shane.holland@ahead.com>
2025-12-10 14:44:47 -0500

    Add Goodbye endpoint

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Hello API", version="1.0.0")
 
-@app.get("/")
+@app.get("/hello")
 def hello():
     return {"message": "Hello, World!"}
+
+@app.get("/goodbye")
+def goodbye():
+    return {"message": "Goodbye, World!"}
